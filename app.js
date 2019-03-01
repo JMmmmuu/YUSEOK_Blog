24386cf21644c7928ad4399472c8b284076dc1b0
JMmmmuu <mkyseok1550@gmail.com>
2019-03-01 21:09:04 +0900

    update

--- app.js
+++ app.js
@@ -16,21 +16,70 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(expressSanitizer());
 app.use(methodOverride("_method"));
 
-app.get("/", function(req, res) {
-    res.render("main");
-});
-
 // MONGOOSE
 var postSchema = new mongoose.Schema({
     title: String,
-    titleImage: String,
+    image: String,
     content: String,
     date: {type: Date, default: Date.now}
 });
 var Post = mongoose.model("Post", postSchema);
 
+// ROUTES
+app.get("/", function(req, res) {
+    res.redirect("/jmmmmuu");
+});
 
+app.get("/jmmmmuu", function(req, res) {
+    res.render("main");
+});
 
+/*********************************************************
+ ************************* POST **************************
+ *********************************************************/
+app.get("/jmmmmuu/post", function(req, res) {
+    Post.find({}, function(err, posts) {
+        if (err) {
+            console.log("Error occured while finding posts");
+            console.log(err);
+        }
+        else {
+            res.render("post", {posts: posts});
+        }
+    });
+});
+
+app.get("/jmmmmuu/post/new", function(req, res) {
+    res.render("newPost");
+});
+
+app.post("/jmmmmuu/post", function(req, res) {
+    req.body.post.content = req.sanitize(req.body.post.content);
+    Post.create(req.body.post, function(err, newPost) {
+        if (err) {
+            console.log("error in posting");
+            console.log(err);
+            res.render("newPost");
+        }
+        else {
+            res.redirect("/jmmmmuu/post");
+        }
+    });
+});
+
+/*********************************************************
+ ************************* BOOK **************************
+ *********************************************************/
+app.get("/jmmmmuu/book", function(req, res) {
+    res.render("book");
+});
+
+/*********************************************************
+ ************************ RUNNING ************************
+ *********************************************************/
+app.get("/jmmmmuu/running", function(req, res) {
+    res.render("running");
+});
 
 
 
