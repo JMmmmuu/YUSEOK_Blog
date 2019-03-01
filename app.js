f0ce0131e3e85f146ce7c82257c01b881f4b5863
JMmmmuu <mkyseok1550@gmail.com>
2019-03-01 13:42:12 +0900

    change port

--- app.js
+++ app.js
@@ -23,6 +23,6 @@ app.get("/", function(req, res) {
 
 
 
-app.listen(process.env.PORT || 3000, function() {
+app.listen(process.env.PORT || 5000, function() {
     console.log("server has started");
 });
