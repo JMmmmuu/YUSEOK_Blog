16e5c740773a0c26491a2430298bfc8f93ffb0d8
JMmmmuu <mkyseok1550@gmail.com>
2019-03-01 13:05:52 +0900

    blog app initialize

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    methodOverride = require("method-override"),
+    expressSanitizer = require("express-sanitizer");
+
+// APP CONFIG
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
+app.use(methodOverride("_method"));
+
+app.get("/", function(req, res) {
+    res.render("main");
+});
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT, function() {
+    console.log("server has started");
+});
