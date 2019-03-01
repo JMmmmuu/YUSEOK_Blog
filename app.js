f70741a3a29390c2e845312441f675dc6fa7c893
JMmmmuu <mkyseok1550@gmail.com>
2019-03-01 17:18:43 +0900

    add mongodb url

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ var express = require("express"),
     expressSanitizer = require("express-sanitizer");
 
 // APP CONFIG
+var uriString = process.env.MONGOLAB_URI ||
+                process.env.MONGOHQ_URL ||
+                "mongodb://localhost/yuseok_blog";
+mongoose.connect(uriString, {useNewUrlParser: true});
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -15,6 +19,16 @@ app.use(methodOverride("_method"));
 app.get("/", function(req, res) {
     res.render("main");
 });
+
+// MONGOOSE
+var postSchema = new mongoose.Schema({
+    title: String,
+    titleImage: String,
+    content: String,
+    date: {type: Date, default: Date.now}
+});
+var Post = mongoose.model("Post", postSchema);
+
 
 
 
