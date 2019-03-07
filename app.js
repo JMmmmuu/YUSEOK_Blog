9030ed722a459cbfe2dee2cd69d294948521e44d
JMmmmuu <mkyseok1550@gmail.com>
2019-03-07 09:25:22 +0900

    npm passport stuffs

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var express = require("express"),
     mongoose = require("mongoose"),
     methodOverride = require("method-override"),
     expressSanitizer = require("express-sanitizer");
+var passport = require("passport"),
+    LocalStrategy = require("passport-local");
 
 // APP CONFIG
 var uriString = "mongodb://jmmmmuu:yuseok123@ds157677.mlab.com:57677/heroku_5ws4pk3r";
@@ -15,7 +17,8 @@ app.use(expressSanitizer());
 app.use(methodOverride("_method"));
 
 // MONGOOSE
-var Post = require("./models/post");
+var Post = require("./models/post"),
+    User = require("./models/user");
 
 // ROUTES
 var indexRoutes = require("./routes/index"),
@@ -31,7 +34,17 @@ app.use("/jmmmmuu/post", postRoutes);
 app.use("/jmmmmuu/book", bookRoutes);
 app.use("/jmmmmuu/running", runningRoutes);
 
-
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "Welcome to YUSEOK_WOLRD, full of myself!",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
 app.listen(process.env.PORT || 5000, function() {
