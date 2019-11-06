7a6caa75cc47c0a85540e45cc8fa1a69b5823fd7
JMmmmuu <mkyseok1550@gmail.com>
2019-11-06 13:09:12 +0900

    add Book model in the main page

--- routes/index.js
+++ routes/index.js
@@ -28,7 +28,8 @@ router.get("/", function(req, res) {
             recentBooks = books;
         }
     });
-    res.render("main", {'posts':recentPosts, 'books':recentBooks});
+    // res.render("main", {posts:recentPosts, books:recentBooks});
+    res.render("main", {posts, recenPosts});
 });
 
 // REGISTER ROUTE
